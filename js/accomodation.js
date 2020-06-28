d597356b433c67c2bdcd9dec8d0cbcd619de5b4f
Yuri Golikov <meetmatt@ya.ru>
2020-06-28 21:24:25 +0200

    Added accommodation form (#28)
    * Added accommodation form
    
    * Fixed CSS link

---
+++ js/accomodation.js
@@ -0,0 +1,16 @@
+Date.prototype.toDateInputValue = (function () {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0, 10);
+});
+Date.prototype.addDays = function (days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
+window.onload = function () {
+    document.getElementById('date-period-from').value = new Date().addDays(1).toDateInputValue();
+    document.getElementById('date-period-to').value = new Date().addDays(5).toDateInputValue();
+};
+
